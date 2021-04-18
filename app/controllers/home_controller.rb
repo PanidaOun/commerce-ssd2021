ee1a0de8f9cd1927020fe9de5ea55ff9dc613f56
PanidaOun <panida.ou@ku.th>
2021-04-19 01:27:01 +0700

    Improve homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @search = params[:search]
+    @products = Product.all
+    @products = @products
+      .search(@search) if @search.present?
+    @products = @products.page(params[:page]).per(4)
   end
 end
